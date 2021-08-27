a02ae0dd5d483f94619d5aae42763eede012c25b
uzimaru0000 <shuji365630@gmail.com>
2021-08-28 07:18:54 +0900

    :sparkles: impl help message

--- src/input.rs
+++ src/input.rs
@@ -1,15 +1,19 @@
 use anyhow::Result;
-use tokio::io::{AsyncReadExt, BufReader, stdin};
+use tokio::io::{stdin, AsyncReadExt, BufReader};
 
 pub async fn read<R: AsyncReadExt + std::marker::Unpin>(
-	reader: &mut BufReader<R>,
+    reader: &mut BufReader<R>,
 ) -> Result<String> {
-	let mut buf = String::new();
-	reader.read_to_string(&mut buf).await?;
-	Ok(buf)
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf).await?;
+    Ok(buf)
 }
 
 pub async fn read_stdin() -> Result<String> {
-	let mut reader = BufReader::new(stdin());
-	read(&mut reader).await
+    let mut reader = BufReader::new(stdin());
+    read(&mut reader).await
+}
+
+pub fn is_pipe() -> bool {
+    atty::isnt(atty::Stream::Stdin)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,48 @@
-pub mod input;
-pub mod data;
-pub mod utils;
+use anyhow::Result;
+use clap::{App, Arg};
+use tokio::io::BufReader;
+
+mod data;
+mod input;
+mod utils;
+
+pub struct Application<'a, 'b> {
+    app: App<'a, 'b>,
+}
+
+impl<'a, 'b> Application<'a, 'b> {
+    pub fn new() -> Self {
+        let app = App::new("tv")
+            .version(env!("CARGO_PKG_VERSION"))
+            .author(env!("CARGO_PKG_AUTHORS"))
+            .about("Format json and csv into table view")
+            .arg(Arg::with_name("PATH").help("").index(1));
+
+        Self { app }
+    }
+
+    pub async fn run(&mut self) -> Result<()> {
+        let matcher = self.app.clone().get_matches();
+        let path = matcher.value_of("PATH").map(String::from);
+
+        if path.is_none() && !input::is_pipe() {
+            let mut out = std::io::stdout();
+            self.app.write_long_help(&mut out)?;
+            return Ok(());
+        }
+
+        let raw = match path {
+            Some(p) => {
+                let file = tokio::fs::File::open(String::from(p)).await?;
+                let mut reader = BufReader::new(file);
+                input::read(&mut reader).await
+            }
+            None => input::read_stdin().await,
+        }?;
+
+        let data = data::Data::from(&raw)?;
+        println!("{}", data);
+
+        Ok(())
+    }
+}
